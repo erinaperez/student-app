845bea20c01761c96b0aed4d343f05bf2519ffcf
Erina Perez <erinaperez@gmail.com>
2023-06-12 22:27:34 -0700

    Nested components

---
+++ src/components/StudentList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './StudentList.css';
+import Student from './Student'
+
+const StudentList = () => {
+    const headingClass = 'student-list__heading';
+    const listClass = 'student-list';
+
+    return (
+        <section>
+            <h2 className={headingClass}>Student List</h2>
+            <ul className={listClass}>
+                <li><Student></Student></li>
+                <li><Student></Student></li>
+                <li><Student></Student></li>
+            </ul>
+        </section>
+    );
+};
+
+export default StudentList;
